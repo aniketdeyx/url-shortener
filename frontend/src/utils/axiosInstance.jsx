2c05ca5d64ca0726586f9001816335db7e7d9364
ANIKET DEY <aniketdey@ANIKETs-MacBook-Air.local>
2025-05-29 12:56:14 +0530

    fixxedd

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,6 +1,14 @@
-import axios from "axios"
+// api.js or axiosInstance.js
+import axios from "axios";
+
+// Set base URL based on environment
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000" // local backend
+    : "/"; // for production, backend is proxied or on same domain
+
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
-    timeout:10000, //10s
-    withCredentials:true
-})
+  baseURL: BASE_URL,
+  timeout: 10000,
+  withCredentials: true,
+});
